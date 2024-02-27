194af9a830663b05f9d6cbcee879d20c96a9b8be
Igor Roiz <igorrt66@gmail.com>
2024-02-27 16:36:39 -0300

    Criação de User Roles

---
+++ smart-flow/smart-flow/src/main/java/com/smartflow/smartflow/model/Roles.java
@@ -0,0 +1,31 @@
+package com.smartflow.smartflow.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "roles")
+@Setter
+@Getter
+public class Roles {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+
+}
--- smart-flow/smart-flow/src/main/java/com/smartflow/smartflow/model/User.java
+++ smart-flow/smart-flow/src/main/java/com/smartflow/smartflow/model/User.java
@@ -1,12 +1,19 @@
 package com.smartflow.smartflow.model;
 
 import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -33,6 +40,10 @@ public class User {
 
     @Column(name = "registrationDate")
     private Timestamp registrationDate;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "userId"), inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
+    private List<Roles> roles = new ArrayList<>();
 
     public User() {
     }
