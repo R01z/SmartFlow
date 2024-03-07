882aa4cb562274071bc170f2588ef131bba808b7
Igor Roiz <igorrt66@gmail.com>
2024-03-07 11:27:45 -0300

    Estilizando janela de cadastro

--- smart-flow_front/src/app/user-register/user-register.component.ts
+++ smart-flow_front/src/app/user-register/user-register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+
 @Component({
   selector: 'app-user-register',
   templateUrl: './user-register.component.html',
