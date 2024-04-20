a5a35c4e1f87b58d74fd44deffb98f35261af62c
Igor Roiz <igorrt66@gmail.com>
2024-04-20 11:08:18 -0300

    Create Information.java

---
+++ smart-flow/smart-flow/src/main/java/com/smartflow/smartflow/model/Information.java
@@ -0,0 +1,69 @@
+package com.smartflow.smartflow.model;
+
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "information")
+@Getter
+@Setter
+public class Information {
+
+    @Id
+    @Column(name = "informationId")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer informationId;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "uploadDate")
+    private Timestamp uploadDate;
+
+    @Lob
+    @Column(name = "file")
+    private byte[] file;
+
+    @Column(name = "link")
+    private String link;
+
+    @ManyToOne
+    @JoinColumn(name = "teamId")
+    private Teams team;
+
+    @ElementCollection
+    private List<String> tags;
+
+    public Information() {
+    }
+
+    public Information(Integer informationId, String name, String description, Timestamp uploadDate, byte[] file,
+            String link, Teams team, List<String> tags) {
+        this.informationId = informationId;
+        this.name = name;
+        this.description = description;
+        this.uploadDate = uploadDate;
+        this.file = file;
+        this.link = link;
+        this.team = team;
+        this.tags = tags;
+    }
+
+}
