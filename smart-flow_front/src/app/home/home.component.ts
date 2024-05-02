630517ef1d8090bf0d224bd257634ce0c2cd336f
Igor Roiz <igorrt66@gmail.com>
2024-05-02 16:19:24 -0300

    Pesquisa por informações na página inicial (Falta CSS)

--- smart-flow_front/src/app/home/home.component.ts
+++ smart-flow_front/src/app/home/home.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../user.service';
+import { InformationFilter } from '../models/information-filter.model';
+import { Information } from '../models/information.model';
+import { InformationService } from '../information.service';
+import { Team } from '../models/team.model';
 
 @Component({
   selector: 'app-home',
@@ -9,12 +13,44 @@ import { UserService } from '../user.service';
 
 export class HomeComponent implements OnInit {
   userName:string = '';
+  searchFilters: InformationFilter = {};
+  showResults: boolean = false;
+  informationResults: Information[] = [];
+  userTeams: Team[] = [];
+  selectedTeams: number[] = [];
+  tags: string = '';
 
-  constructor(private userService: UserService) { }
+  constructor(private userService: UserService, private informationService: InformationService) { }
 
   ngOnInit(): void {
     this.userService.getLoggedUser().subscribe((user: any) => {
       this.userName = user.name;
+      this.loadUserTeams(user.id);
+    });
+  }
+
+  loadUserTeams(userId: number): void {
+    this.userService.getUserTeams(userId).subscribe((teams: Team[]) => {
+      this.userTeams = teams;
+    });
+  }
+
+  processTags(): void {
+    // Verifica se há tags inseridas no campo de pesquisa
+    if (this.tags) {
+      // Separar as tags por vírgula e remover espaços em branco
+      this.searchFilters.tags = this.tags.split(',').map(tag => tag.trim());
+    }
+  }
+
+  search(): void {
+    this.processTags();
+    this.searchFilters.teamId = this.selectedTeams.length > 0 ? this.selectedTeams : undefined;
+
+    // Realizar busca com base nos filtros preenchidos
+    this.informationService.searchInformations(this.searchFilters).subscribe((results: Information[]) => {
+      this.informationResults = results;
+      this.showResults = true;
     });
   }
 }
--- smart-flow_front/src/app/information.service.ts
+++ smart-flow_front/src/app/information.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { InformationFilter } from './models/information-filter.model';
 import { Information } from './models/information.model';
@@ -12,7 +12,29 @@ export class InformationService {
   constructor(private http: HttpClient) { }
 
   searchInformations(filters: InformationFilter): Observable<Information[]> {
-    return this.http.post<Information[]>('http://localhost:8090/api/v1/information/getInformations', filters);
+    // Criar parâmetros de consulta a partir dos filtros
+    let params = new HttpParams();
+    if (filters.name) {
+      params = params.set('name', filters.name);
+    }
+    if (filters.description) {
+      params = params.set('description', filters.description);
+    }
+    if (filters.startDate) {
+      params = params.set('startDate', filters.startDate.toISOString());
+    }
+    if (filters.endDate) {
+      params = params.set('endDate', filters.endDate.toISOString());
+    }
+    if (filters.teamId && filters.teamId.length > 0) {
+      params = params.set('teamId', filters.teamId.join(','));
+    }
+    if (filters.tags && filters.tags.length > 0) {
+      params = params.set('tags', filters.tags.join(','));
+    }
+
+    // Fazer a solicitação GET com os parâmetros de consulta
+    return this.http.get<Information[]>('http://localhost:8090/api/v1/information/getInformations', { params: params });
   }
 
 }
