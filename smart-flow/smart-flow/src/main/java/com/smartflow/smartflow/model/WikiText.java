7437361e079fbe71458edd2c8fcb3aad7b733457
Igor Roiz <igorrt66@gmail.com>
2024-05-10 18:38:48 -0300

    Definindo tamanho do texto da wiki como 10k

--- smart-flow/smart-flow/src/main/java/com/smartflow/smartflow/model/WikiText.java
+++ smart-flow/smart-flow/src/main/java/com/smartflow/smartflow/model/WikiText.java
@@ -24,7 +24,7 @@ public class WikiText {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer wikiId;
 
-    @Column(name = "text")
+    @Column(name = "text", length = 10000)
     private String text;
 
     @ManyToOne
